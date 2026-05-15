c5aaa1fee215b01a8cf4659606f5abca66e8b07d
Dante <migunenko.1994@gmail.com>
2026-05-15 17:55:47 +0300

    move images to public folder

--- src/data/recent-games.js
+++ src/data/recent-games.js
@@ -1,12 +1,12 @@
 export const recentGamesData = {
     title: ['recent', 'favourite'],
     games: [
-        { title: 'Miss cherry fruits', image: './src/assets/images/games/miss-cherry-fruits.png', playUrl: '#', demoUrl: '#' },
-        { title: "Parrot's gold", image: './src/assets/images/games/parrots-gold.png', playUrl: '#', demoUrl: '#' },
-        { title: "Duel at dawn", image: './src/assets/images/games/duel-at-dawn.png', playUrl: '#', demoUrl: '#' },
-        { title: 'Mechanical clover', image: './src/assets/images/games/mechanical-clover.png', playUrl: '#', demoUrl: '#' },
-        { title: 'Wild cash', image: './src/assets/images/games/wild-cash.png', playUrl: '#', demoUrl: '#' },
-        { title: "Alien fruits", image: './src/assets/images/games/alien-fruits.png', playUrl: '#', demoUrl: '#' },
-        { title: 'Sweet rush', image: './src/assets/images/games/sweet-rush.png', playUrl: '#', demoUrl: '#' },
+        { title: 'Miss cherry fruits', image: '/images/games/miss-cherry-fruits.png', playUrl: '#', demoUrl: '#' },
+        { title: "Parrot's gold", image: '/images/games/parrots-gold.png', playUrl: '#', demoUrl: '#' },
+        { title: "Duel at dawn", image: '/images/games/duel-at-dawn.png', playUrl: '#', demoUrl: '#' },
+        { title: 'Mechanical clover', image: '/images/games/mechanical-clover.png', playUrl: '#', demoUrl: '#' },
+        { title: 'Wild cash', image: '/images/games/wild-cash.png', playUrl: '#', demoUrl: '#' },
+        { title: "Alien fruits", image: '/images/games/alien-fruits.png', playUrl: '#', demoUrl: '#' },
+        { title: 'Sweet rush', image: '/images/games/sweet-rush.png', playUrl: '#', demoUrl: '#' },
     ]
 };
